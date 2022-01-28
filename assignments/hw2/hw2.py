373f6eeb89cc0faae1b7b12c304cca9468698448
Lane Ellisor <43457662+LaneEllisor@users.noreply.github.com>
2022-01-28 12:27:02 -0500

    aye yuh i'm submitting

--- assignments/hw2/hw2.py
+++ assignments/hw2/hw2.py
@@ -1,19 +1,19 @@
 """
-Name: ChapmanEllisor
+Name: Chapman Ellisor
 hw2.py
 
-Problem:
+Problem: For loops and simple scripts exercises.
 Certification of Authenticity: I certify that this assignment is entirely my own work.
 """
-
+# Importing Libraries
 import math
 
+# Defining Functions
 def sum_of_threes():
     upper_bound = eval(input("Enter the Upper Bound"))
     sum = 0
     for i in range(0, upper_bound+1):
         if (i%3==0):
-            print(i)
             sum = sum + i
     print("The sum of threes is", sum)
 
@@ -34,18 +34,20 @@ def triangle_area():
     print("The area is", Area)
 
 def sum_squares():
-    lower_range = eval(input("Enter Lower Range:"))
-    upper_range = eval(input("Enter Upper Range"))
+    lower_range = eval(input("Enter Lower Range: "))
+    upper_range = eval(input("Enter Upper Range: "))
 
+    squares = 0
+    for i in range(lower_range, upper_range+1):
+        squares = squares + i**2
+    print(squares)
 
 def power():
-    base = eval(input("base="))
-    power = eval(input("power="))
+    base = eval(input("base= "))
+    power = eval(input("power= "))
 
     power_ab = base**power
     print(base, "^", power, "=", power_ab)
 
 if __name__ == '__main__':
     pass
-
-
