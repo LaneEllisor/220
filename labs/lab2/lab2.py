01528d323f3bb18a9c6e3682f15c0f96669d4197
Lane Ellisor <43457662+LaneEllisor@users.noreply.github.com>
2022-01-31 14:43:48 -0500

    notes from jan 31

---
+++ labs/lab2/lab2.py
@@ -0,0 +1,19 @@
+"""
+# Name: Chapman Lane Ellisor
+# lab2.py
+# Problem: Statistical Analysis of Input Values
+# Certification of Authenticity: I certify that this assignment is entirely my own work.
+"""
+
+# getting user inputs
+nums = 0
+sum = 0
+
+for n in range(nums):
+    values = float(input("Enter Value: "))
+    sum += values
+
+length = length(nums)
+
+#arithemetic mean
+mean = sum / num
