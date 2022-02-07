296aa9419fd83e4872d427a0bb5a45accf4558da
Lane Ellisor <43457662+LaneEllisor@users.noreply.github.com>
2022-02-07 14:44:02 -0500

    wawa pretzels are life

--- labs/lab3/lab3.py
+++ labs/lab3/lab3.py
@@ -1,7 +1,8 @@
-from math import *
+
 
 def average_vehicles():
     roads_surveyed = int(input("How many roads were surveyed? "))
+    total_vehicles = 0
     for i in range(roads_surveyed):
         print("How many days was road", i+1, end=' ')
         days_surveyed = int(input("surveyed?   "))
@@ -10,11 +11,10 @@ def average_vehicles():
             print("How many cars traveled on day", k+1, end='')
             cars_per_day = int(input("?  "))
             sum += cars_per_day
+        cars_travelled = sum
         average_veh = sum / (k+1)
         print("Road", i + 1, "average vehicles per day:", round(average_veh,3))
-
-    total_vehicles = 0
-    total_vehicles +=
+        total_vehicles += cars_travelled
 
     average_vehicles_per_road = total_vehicles/roads_surveyed
     print("Total number of vehicles traveled on all roads:", total_vehicles)
