eaa84cb3c9de6936217bfe4272b5433eb8180aa7
Lane Ellisor <43457662+LaneEllisor@users.noreply.github.com>
2022-02-18 12:24:18 -0500

    its a day

--- assignments/hw5/hw5.py
+++ assignments/hw5/hw5.py
@@ -31,9 +31,69 @@ def company_name():
 
 #company_name()
 
+def initials():
+    total_students = int(input("How many students are in the class?  "))
+    for i in range(total_students):
+        name = input("What is the name of student "+ str(i+1)+ " ?  ")
+
+        space = name.find(" ")
+        first_name = name[0:space]
+        last_name = name[space + 1:]
+
+        first_caps = first_name[0].upper()
+        last_caps = last_name[0].upper()
+        print(first_caps+last_caps)
+#initials()
+
+def names():
+    names = input("Enter a list of names: ")
+    name_list = names.split(',')
+
+    print(name_list)
+
+    for i in range(len(name_list)):
+        #splits = name_list[i].split("  ")
+
+        space = name_list[i].find(" ")
+        first_name = name_list[0:space]
+        last_name = name_list[space + 1:]
+
+        print(space)
+
+#names()
+
+def thirds():
+    num_sent = int(input("Enter number of sentences:  "))
+    third = ""
+    for i in range(num_sent):
+        sentences = str(input("enter sentence "+str(i+1)+": "))
+
+        third += sentences[::3]+"\n"
+    print(third)
 
 
 
+thirds()
+
+def word_average():
+    sentence = str(input("Enter a sentence: "))
+    words = sentence.split()
+    avg = sum(len(word) for word in words) / len(words)
+    print(avg)
+
+#word_average()
+
+def pig_latin():
+    sentence = str(input("Enter a sentence: "))
+    words = sentence.split()
+    newSentence = ""
+    for i in words:
+        newSentence += i[1:]+i[0]+"ay "
+        newSentence = newSentence.lower()
+        newSentence = newSentence.capitalize()
+
+    print(newSentence)
+#pig_latin()
 
 
 if __name__ == '__main__':
