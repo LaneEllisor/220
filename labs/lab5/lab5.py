4ba8899c5feb891761090cb50aa313b2efce76f2
Lane Ellisor <43457662+LaneEllisor@users.noreply.github.com>
2022-02-21 15:24:12 -0500

    h

--- labs/lab5/lab5.py
+++ labs/lab5/lab5.py
@@ -1,5 +1,61 @@
-def triange():
-    pass
+from graphics import *
+from math import *
+
+
+def triangle():
+    # create window
+    win_width = 400
+    win_height = 400
+    win = GraphWin("Color Shape", win_width, win_height)
+    win.setBackground("white")
+
+    # displaying closing message
+    text1 = Text(Point(200, 350), "Click again to close")
+    text1.setSize(8)
+    text1.draw(win)
+
+    p1 = win.getMouse()
+    p1.draw(win)
+    p2 = win.getMouse()
+    p2.draw(win)
+    p3 = win.getMouse()
+    p3.draw(win)
+
+    triangle = Polygon(p1, p2, p3)
+    triangle.draw(win)
+
+    #input("hit enter to exit")
+
+    x1 = p1.getX()
+    x2 = p2.getX()
+    x3 = p3.getX()
+
+    y1 = p1.getY()
+    y2 = p2.getY()
+    y3 = p3.getY()
+
+    dx1 = x2-x1
+    dy1 = y2-y1
+
+    dx2 = x3-x2
+    dy2 = y3-y2
+
+    dx3 = x3-x1
+    dy3 = y3-y1
+
+    length1 = sqrt(dx1**2+dy1**2)
+    length2 = sqrt(dx2**2+dy2**2)
+    length3 = sqrt(dx3**2+dy3**2)
+
+    perimeter = length1+length2+length3
+    print("The perimeter is: ", round(perimeter,3))
+
+    s = (perimeter)/2
+    area = sqrt(s*(s-length1)*(s-length2)*(s-length3))
+    print("The area is: ", round(area,3))
+
+#triangle()
+
 
 
 def color_shape():
@@ -42,11 +98,15 @@ def color_shape():
     green_text.draw(win)
     blue_text.draw(win)
 
+
+
     # Wait for another click to exit
     win.getMouse()
     win.close()
 
 
+
+color_shape()
 def process_string():
     pass
 
