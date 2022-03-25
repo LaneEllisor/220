064d1de084c89a1a8da22cd6dd30812822c96b03
laneellisor <claneellisor@gmail.com>
2022-03-24 23:45:27 -0400

    yuh

--- assignments/hw9/hw9.py
+++ assignments/hw9/hw9.py
@@ -7,19 +7,32 @@ Problem: Hangman
 Certification of Authenticity:
 I certify that this assignment is entirely my own work.
 """
-
+from random import randint
 def get_words(file_name):
     file = open(file_name, "r")
+    lines = file.readlines()
     words = []
-    for line in file:
-        words.append(line.strip())
+    for line in lines:
+        words.append(line)
+    file.close
     return words
 
 def get_random_word(words):
-    random_word = words[randint(1, len(words))]
+    random_word = str(words[randint(0,len(words)-1)])
     return random_word
 
 
-def letter_in_secret_word(letter)
+def letter_in_secret_word(letter, secret_word):
+    for i in range(len(secret_word)):
+        if secret_word[i] == letter:
+            return True
+        else:
+            return False
 
 
+def already_guessed(letter, guesses):
+    for letter in guesses:
+        if letter in guesses:
+            return True
+        else:
+            return False
