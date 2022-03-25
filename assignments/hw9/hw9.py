f98d5a56af57423eea0b8f2582be13ce07e4e96f
laneellisor <claneellisor@gmail.com>
2022-03-24 23:51:47 -0400

    yuh

--- assignments/hw9/hw9.py
+++ assignments/hw9/hw9.py
@@ -36,3 +36,25 @@ def already_guessed(letter, guesses):
             return True
         else:
             return False
+
+
+def make_hidden_secret(secret_word, guesses):
+    pass
+
+
+def won(guessed):
+    pass
+
+
+def play_graphics(secret_word):
+    pass
+
+
+def play_command_line(secret_word):
+    pass
+
+
+if __name__ == '__main__':
+    pass
+    # play_command_line(secret_word)
+    # play_graphics(secret_word)
