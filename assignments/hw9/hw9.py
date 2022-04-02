ed341e12fbc98255e3814a2c155ed2bcbcaba85e
laneellisor <claneellisor@gmail.com>
2022-04-01 22:53:21 -0400

    smh

--- assignments/hw9/hw9.py
+++ assignments/hw9/hw9.py
@@ -8,18 +8,18 @@ Certification of Authenticity:
 I certify that this assignment is entirely my own work.
 """
 from random import randint
+
 def get_words(file_name):
     file = open(file_name, "r")
-    lines = file.readlines()
     words = []
-    for words in lines:
-        words.append(words)
+    for word in file:
+        words.append(word)
     return words
 
 def get_random_word(words):
     random_index = randint(0, len(words)-1)
     random_word = words[random_index]
-    return random_word
+    return random_word[:-1]
 
 
 def letter_in_secret_word(letter, secret_word):
@@ -38,13 +38,13 @@ def already_guessed(letter, guesses):
 
 def make_hidden_secret(secret_word, guesses):
     hidden_word = ""
-    for letter in range(len(secret_word)):
-        if letter not in guesses:
-            secret_word = secret_word.replace(letter, "_")
+    for letter in secret_word:
+        if letter in guesses:
+            pass
         else:
-            return None
-    for letter in range(len(secret_word)):
-        hidden_word = " ".join(hidden_word)
+            secret_word = str(secret_word.replace(letter, "_ "))
+    for letter in secret_word:
+        hidden_word += letter + " "
     return hidden_word
 
 
@@ -60,7 +60,35 @@ def play_graphics(secret_word):
 
 
 def play_command_line(secret_word):
-    pass
+    attempts = 6
+    guess_list = []
+
+    hidden_word = make_hidden_secret(secret_word, guesses)
+
+    while not won(hidden_word) and attempts > 0:
+        guessed_letter = input("Guess a letter:   ")
+
+        if not already_guessed(letter, guesses) and letter in secret_word:
+            guess_list.append(letter)
+            hidden_word = make_hidden_secret(secret_word, guesses)
+            return hidden_word
+        if not already_guessed(letter, guesses) and letter not in secret_word:
+            guessed_list.append(letter)
+            attempts -= 1
+            hidden_word = make_hidden_secret(secret_word, guesses)
+
+            return hidden_word
+
+        else:
+            return "Error"
+
+    if won(hidden_word):
+        return "You won!"
+    if attempts == 0:
+        return "You lost. The word was" + str(secret_word)
+    else:
+        pass
+
 
 
 if __name__ == '__main__':
