b09e86d0382b95bca1ac2c6ec71fe7cc9ac77b0d
Lane Ellisor <43457662+LaneEllisor@users.noreply.github.com>
2022-04-18 23:12:18 -0400

    Update algorithms.py
    git is still not working

--- labs/lab12/algorithms.py
+++ labs/lab12/algorithms.py
@@ -9,3 +9,9 @@ def is_in_linear(search_val, values):
     while count < len(values) and search_val != values[count]:
         count += 1 
         return count < len(values)
+ 
+def find_and_remove_first(list, value):
+    list_index = list.index(value)
+    list.pop(list_index)
+    list.insert(list_index, 'Lane')
+    return list
